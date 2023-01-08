ed9c943e29cdeb3c80bd046d5610cd69d2a6454b
vinicius <vinicius.cardoso8888@outlook.com>
2023-01-07 22:07:57 -0300

    passando valores para o input

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
 const listateclas = document.querySelectorAll('.tecla');
-const input1 = document.querySelector('#array-title');
- input1.value = mostracalorias;
-let cont=0;
-while(cont < listateclas.length){
-    listateclas[cont].onclick = mostracalorias;
-    cont = cont + 1;
-    console.log(cont);
-}
+console.log(listateclas)
+const input1 = document.querySelector('#visor');
+//input1.textContent = mostrarCalorias;
 
- function mostracalorias(){
-     const listateclas = document.querySelector('.tecla').value;
-     alert(listateclas);
+ function mostrarCalorias(idvalue){
+input1.value = document.querySelector(idvalue).value;
 
  }
+ 
+
+let cont=0;
+ while(cont < listateclas.length){
+     const tecla = listateclas[cont];
+     let valores = tecla.classList[1];
+     const idvalores = `.tecla ${valores}`;
+     console.log(idvalores);
+     tecla.onclick = function (){
+         mostrarCalorias(idvalores);
+     };
+     cont = cont + 1;
+ }
+    
+//  function valorcalorias(idtecla){
+     ''
+     
+//     alert(document.querySelector(idtecla).value);
+     
+
+//  }
+ 
+//  function mostra(){
+
+//  }
+
+//  function tecla (num){
+//      document.querySelector('#visor').value = document.querySelector('#visor').value + num;
+
+//  }
+ 
 
 // function calculo(){
 //     const teclas = document.querySelector('.tecla').value;
